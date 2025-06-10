4e48fb105f9a06cd8e997c6ad909003fe6b5b049
xunyuanxin <xunyuanxin@163.com>
2025-06-10 18:05:49 -0400

    fix app test js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
+
+jest.mock('./Fib', () => () => <div>Mocked Fib</div>);
 import App from './App';
 
-jest.mock('axios');
-
-test('renders learn react link', () => {});
+test('renders learn react link', () => {
+  render(<App />);
+  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
+});
